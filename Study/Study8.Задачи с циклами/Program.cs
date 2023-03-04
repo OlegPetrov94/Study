e4f614a820f63e2cf760d82d94d71a2189334636
DESKTOP-CT047HR\Юля <alegpetrov94@gmail.com>
2023-03-04 20:18:26 +0300

    Начали смотреть цикл "for" начали решать задачи

---
+++ Study/Study8.Задачи с циклами/Program.cs
@@ -0,0 +1,24 @@
+﻿//SimpleFor();
+
+Console.WriteLine("*");
+Console.WriteLine("**");
+Console.WriteLine("***");
+Console.WriteLine("****");
+Console.WriteLine("*****");
+
+static void SimpleFor()
+{
+    // сколько раз вывести приветствие
+
+
+    Console.WriteLine("Введите число: ");
+    string ageS = Console.ReadLine();
+    int age = Convert.ToInt32(ageS);
+
+    for (int i = 0; i < age; i++)
+    {
+        Console.WriteLine("Привет!");
+
+    }
+}
+
