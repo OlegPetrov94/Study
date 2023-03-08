4aeda4cf7feff79a65ff44be0687420f55769247
DESKTOP-CT047HR\Юля <alegpetrov94@gmail.com>
2023-03-08 22:59:39 +0300

    ObratniuOtschet в цикле For

--- Study/Study8.Циклы.Задача/Program.cs
+++ Study/Study8.Циклы.Задача/Program.cs
@@ -1,41 +1,72 @@
-﻿//SimpleFor();
+﻿ObratniuOtschet();
 
-// написать цикл 
-//Console.WriteLine("*");
-//Console.WriteLine("**");
-//Console.WriteLine("***");
-//Console.WriteLine("****");
-//Console.WriteLine("*****");
-
-//using System.Globalization;
-
-Console.WriteLine("Введите число: ");
-string ageS = Console.ReadLine();
-int age = Convert.ToInt32(ageS);
-string text = ""; 
-for (int i = 0; i <= age; i++)
+static void Elka()
 {
-    //text += "*"; 
-    text = text + "*";
-    Console.WriteLine(text);
+    // написать цикл 
+    //Console.WriteLine("*");
+    //Console.WriteLine("**");
+    //Console.WriteLine("***");
+    //Console.WriteLine("****");
+    //Console.WriteLine("*****");
+
+    //using System.Globalization;
+
+    Console.WriteLine("Введите число: ");
+    string ageS = Console.ReadLine();
+    int age = Convert.ToInt32(ageS);
+    string text = "";
+    for (int i = 0; i <= age; i++)
+    {
+        //text += "*"; 
+        text = text + "*";
+        Console.WriteLine(text);
+    }
+    Console.WriteLine();
 }
-Console.WriteLine();
 
-//int n = 10; // количество уровней елки
-//for (int i = 1; i <= n; i++)
-//{
-//    // Отрисовываем отступы перед звездочками
-//    for (int j = n - i; j > 0; j--)
-//    {
-//        Console.Write(" ");
-//    }
+static void ForContinueBreak()
+{
+    //Написать программу которая спрашивает сколько раз нужно пройтись по циклу если число делиться на 3 без остатка, то мы не выводим это число на экран, если число = 16 то цикл дальше не выполняется 
+    Console.WriteLine(" Введите число ");
+    string resault = Console.ReadLine();
+    int resault1 = Convert.ToInt32(resault);
+    for (int i = 1; i < resault1; i++)
+    {
+        if (i == 16)
+        {
+            break;
+        }
 
-//    // Отрисовываем звездочки
-//    for (int k = 1; k <= (2 * i - 1); k++)
-//    {
-//        Console.Write("*");
-//    }
+        if (i % 3 == 0)
+        {
+            continue;
+        }
 
-//    // Переход на новую строку
-//    Console.WriteLine();
-//}
+        Console.WriteLine(i);
+    }
+}
+
+static void ObratniuOtschet()
+{
+    //Программа спрашивает два числа где первое число больше второго числа, если второе число больше первого числа программа сообщает что ошибка. 
+    //если ввод корректен то необходимо отобразить все цифры от первого числа до второго чила в порядке уменьшения
+
+    Console.WriteLine("Введите первое число ");
+    string resault = Console.ReadLine();
+    int resault1 = Convert.ToInt32(resault);
+    Console.WriteLine("Введите второе число ");
+    string resault2 = Console.ReadLine();
+    int resault3 = Convert.ToInt32(resault2);
+
+    if (resault3 > resault1)
+    {
+        Console.WriteLine("Ошибка");
+    }
+    else
+    {
+        for (int i = resault1; i >= resault3; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
